1cf8d73e3e58333773998bdfaf5eb991e76b315a
clark <clark.joao@gmail.com>
2020-04-15 22:17:55 -0300

    Feature/AddTypeOrm

--- src/services/CreateAppointmentServices.ts
+++ src/services/CreateAppointmentServices.ts
@@ -1,30 +1,30 @@
 import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
 import Appointment from '../models/Appointment';
 import AppointmentRepository from '../repositories/appointments.reporitory';
-import { response } from 'express';
 interface Request {
     provider: string;
     date: Date;
 }
 
 class CreateAppointmentService {
-    private appointmentRepository: AppointmentRepository;
-    constructor(appointmentRepository: AppointmentRepository) {
-        this.appointmentRepository = appointmentRepository;
-    }
-    public execute({ provider, date }: Request): Appointment {
+    public async execute({ provider, date }: Request): Promise<Appointment> {
+        const appointmentRepository = getCustomRepository(
+            AppointmentRepository,
+        );
         const appointmentDate = startOfHour(date);
-        const findAppointment = this.appointmentRepository.findByDate(
+        const findAppointment = await appointmentRepository.findByDate(
             appointmentDate,
         );
 
         if (findAppointment) {
             throw Error('This appointment is aready book ');
         }
-        const appointment = this.appointmentRepository.create({
+        const appointment = await appointmentRepository.create({
             provider,
             date: appointmentDate,
         });
+        await appointmentRepository.save(appointment);
         return appointment;
     }
 }
